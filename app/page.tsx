bd10fb99afbc9965bab0e1b096057d5a7b5a96c2
Ivan Martínez <calmartivan@gmail.com>
2024-07-07 21:09:04 +0200

    add trofiplast logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
-// import Image from "next/image";
+import Image from "next/image";
 
 
 export default function Page() {
   return (
     <main /* className="flex min-h-screen flex-col items-center justify-between p-24" */>
-
+      <Image
+        src="/trofiplast_logo.png"
+        width={500}
+        height={500}
+        alt="Trofiplast logo"
+      ></Image>
     </main>
   );
 }
