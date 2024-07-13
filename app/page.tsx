a32f2b50d8e8aac3b4ca6c64f7172fb15ea6bb19
Ivan Martínez <calmartivan@gmail.com>
2024-07-13 23:40:33 +0200

    more custom styling based on tailwind ui template

--- app/page.tsx
+++ app/page.tsx
@@ -12,24 +12,39 @@ export default function Page() {
                 <img className="h-20 w-auto rounded-full" src="trofiplast_logo.png" alt="Trofiplast logo" />
               </a>
             </div>
-            <div className="flex lg:hidden">
+ {/*          <div className="flex lg:hidden">
               <button type="button" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
                 <span className="sr-only">Open main menu</span>
                 <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                 </svg>
               </button>
-            </div>
+            </div> */}
+
             <div className="hidden lg:flex lg:gap-x-12">
-              <a href="#" className="text-sm font-semibold leading-6 text-gray-900">About</a>
-              <a href="#" className="text-sm font-semibold leading-6 text-gray-900">Search the database</a>
-              <a href="#" className="text-sm font-semibold leading-6 text-gray-900">Contribute new entries</a>
-              <a href="#" className="text-sm font-semibold leading-6 text-gray-900">Contact</a>
+              <a href="#" className="text-md font-semibold leading-6 text-gray-900 hover:text-lime-800 group">
+                About
+              <div className="bg-gray-900 h-[3px] w-0 group-hover:w-full transition-all duration-200"></div>
+              </a>             
+              <a href="#" className="text-md font-semibold leading-6 text-gray-900 hover:text-lime-800 group">
+                Search the database
+                <div className="bg-gray-900 h-[3px] w-0 group-hover:w-full transition-all duration-200"></div>
+              </a>
+              <a href="#" className="text-md font-semibold leading-6 text-gray-900 hover:text-lime-800 group">
+                Contribute new entries
+                <div className="bg-gray-900 h-[3px] w-0 group-hover:w-full transition-all duration-200"></div>
+                </a>
+              <a href="#" className="text-md font-semibold leading-6 text-gray-900 hover:text-lime-800 group">
+                Contact
+                <div className="bg-gray-900 h-[3px] w-0 group-hover:w-full transition-all duration-200"></div>
+                </a>
             </div>
-            <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-              <a href="#" className="text-sm font-semibold leading-6 text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
+
+          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+              <a href="#" className="text-sm font-semibold leading-6 text-gray-900"><span aria-hidden="true"></span></a>
             </div>
           </nav>
+          
           {/* <!-- Mobile menu, show/hide based on menu open state. --> */}
           <div className="lg:hidden" role="dialog" aria-modal="true">
          {/*    <!-- Background backdrop, show/hide based on slide-over state. --> */}
@@ -50,7 +65,7 @@ export default function Page() {
               <div className="mt-6 flow-root">
                 <div className="-my-6 divide-y divide-gray-500/10">
                   <div className="space-y-2 py-6">
-                    <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">About</a>
+                    <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">About</a>                   
                     <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Search the database</a>
                     <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Contribute new entries</a>
                     <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Contact</a>
@@ -72,13 +87,13 @@ export default function Page() {
             <div className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#298F14] to-[#FDF8FC] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" /*  style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" */></div>
           </div>
           <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56 relative top-8">
-            <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+    {/*     <div className="hidden sm:mb-8 sm:flex sm:justify-center">
               <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              plastic ingestion by animals <a href="#" className="font-semibold text-indigo-600"><span className="absolute inset-0" aria-hidden="true"></span>Read more <span aria-hidden="true">&rarr;</span></a>
+              plastic ingestion by animals <a href="#" className="font-semibold text-gray-900"><span className="absolute inset-0" aria-hidden="true"></span>Read more <span aria-hidden="true">&rarr;</span></a>
               </div>
-            </div>
+            </div> */}
             <div className="text-center">
-              <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Trofiplast</h1>
+              <h1 className="text-8xl font-bold tracking-tight text-gray-900" /* sm:text-6xl */>Trofiplast</h1>
               <p className="mt-10 text-lg leading-8 text-gray-600">A worldwide database of ingested plastics in animals</p>
              {/*  action buttons */}
               <div className="mt-10 flex items-center justify-center gap-x-6">
@@ -132,10 +147,7 @@ export default function Page() {
               <div className="mt-32 flex items-center justify-center gap-x-6  text-justify">
                 <a href="#" className="text-sm font-semibold leading-6 text-gray-900 text-justify">Learn more about Archipelagos ambiente e sviluppo<span aria-hidden="true">→</span></a>
               </div>
-
           </div>
-
-
 
         <div className="flex justify-center items-center py-24">
         <Image
@@ -147,12 +159,7 @@ export default function Page() {
         ></Image>
 
         </div>
-
       </div>
-
-
-
-
     </>
   );
 }
