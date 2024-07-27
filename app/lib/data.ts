dcd9cc128a03237960523cda299519cce5f8a5ce
Ivan Martinez <ivmartinezc@gmail.com>
2024-07-27 22:37:11 +0200

    add binaryTargets for openssl in schema.prisma, add rest of prisma queries to get fields, add first sketch of query with filters

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -41,3 +41,78 @@ export async function getOrderOptions(){
      })
      return options;
  }
+
+ export async function getFamilyOptions(){
+    const options = await prisma.item.findMany({
+         distinct:['family'],
+         select:{
+            order:true
+         }
+     })
+     return options;
+ }
+
+ export async function getGenusOptions(){
+    const options = await prisma.item.findMany({
+         distinct:['genus'],
+         select:{
+            order:true
+         }
+     })
+     return options;
+ }
+
+ export async function getSpeciesOptions(){
+    const options = await prisma.item.findMany({
+         distinct:['species'],
+         select:{
+            order:true
+         }
+     })
+     return options;
+ }
+
+ export async function getAreaOptions(){
+    const options = await prisma.item.findMany({
+         distinct:['area'],
+         select:{
+            order:true
+         }
+     })
+     return options;
+ }
+
+ export async function getOriginOptions(){
+    const options = await prisma.item.findMany({
+         distinct:['origin'],
+         select:{
+            order:true
+         }
+     })
+     return options;
+ }
+
+ export async function getCountryOptions(){
+    const options = await prisma.item.findMany({
+         distinct:['country'],
+         select:{
+            order:true
+         }
+     })
+     return options;
+ }
+
+ export async function getItemsByFilters({group,order,family,genus,species,area,origin,country}){
+    const items = await prisma.item.findMany({
+        where:{
+            "group": group, 
+            "order": order,         
+            "family": family,        
+            "genus": genus,          
+            "species": species,        
+            "area": area,           
+            "origin": origin,        
+            "country": country              }
+    })
+    return items;
+ }
