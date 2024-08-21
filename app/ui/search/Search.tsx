b5c03b46abb25970b4670a3fa9b59f9a1016ca68
Ivan Martinez <ivmartinezc@gmail.com>
2024-08-21 19:04:07 +0200

    add parameter type and jsx interpolations (SearchResults)

--- app/ui/search/Search.tsx
+++ app/ui/search/Search.tsx
@@ -163,7 +163,7 @@ export default function Search({
         </div>
       </div>
     ) : (
-      <SearchResults/>
+      <SearchResults items={items}/>
     )
   }
 </>
--- app/ui/search/SearchResults.tsx
+++ app/ui/search/SearchResults.tsx
@@ -1,33 +1,32 @@
+import { Item } from "@prisma/client";
 
-export default function SearchResults() {
+export default function SearchResults({ items }: { items: Item[] }) {
   return (
     <>
-    <div className="flex justify-center items-center min-h-screen">
-    <table className="table-auto border-collapse border border-gray-300">
-      <thead>
-        <tr>
-          <th className="px-4 py-2 border border-gray-300">Species</th>
-          <th className="px-4 py-2 border border-gray-300">Area</th>
-          <th className="px-4 py-2 border border-gray-300">Country</th>
-        </tr>
-      </thead>
-      
-      <tbody>
-        <tr>
-          <td className="px-4 py-2 border border-gray-300">Species 1</td>
-          <td className="px-4 py-2 border border-gray-300">Area 1</td>
-          <td className="px-4 py-2 border border-gray-300">Country 1</td>
-        </tr>
-        <tr>
-          <td className="px-4 py-2 border border-gray-300">Species 2</td>
-          <td className="px-4 py-2 border border-gray-300">Area 2</td>
-          <td className="px-4 py-2 border border-gray-300">Country 2</td>
-        </tr>
-      
-      </tbody>
-    </table>
-  </div>
-  </>
-  
-  )
+      <div className="flex justify-center items-center min-h-screen">
+        <table className="table-auto border-collapse border border-gray-300">
+          <thead>
+            <tr>
+              <th className="px-4 py-2 border border-gray-300">Species</th>
+              <th className="px-4 py-2 border border-gray-300">Area</th>
+              <th className="px-4 py-2 border border-gray-300">Country</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {items &&
+              items.map((item) => {
+                return (
+                  <tr>
+                    <td className="px-4 py-2 border border-gray-300">{item.species}</td>
+                    <td className="px-4 py-2 border border-gray-300">{item.area}</td>
+                    <td className="px-4 py-2 border border-gray-300">{item.country}</td>               
+                 </tr>
+                );
+              })}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
 }
